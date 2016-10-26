63e07732ec61454d86256bbc598c7c257f87799f
Sondre Stai <sondre.stai@gmail.com>
2016-10-26 13:21:11 +0200

    Contact og form validator

---
+++ final/pages/email.php
@@ -0,0 +1,5 @@
+<?php
+    mail('sondre.stai@gmail.com', $_POST['name'],$_POST['email'],$_POST['henvendelse'],
+     $_POST[getElementById(string $message)]);
+?>
+<p>Din hendvendelse har blitt sent</p>
