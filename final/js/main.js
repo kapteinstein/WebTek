a4b5632e8b56706a5ce231579520d5fb46251ace
torjuss <torjuss@stud.ntnu.no>
2016-11-04 01:12:51 +0100

    Added slide show feature

--- final/js/main.js
+++ final/js/main.js
@@ -1,46 +1,80 @@
-/**
- * This is the main script for the site.
- */
+// This is the main script.
 
-var ul = document.getElementById('nav-list');
+// Slide show, based on the following tutorials:
+// http://www.w3schools.com/howto/howto_js_slideshow.asp
+// http://www.w3schools.com/howto/howto_css_modal_images.asp
+// http://www.w3schools.com/howto/howto_js_lightbox.asp
 
-/**
- * When the html page is loaded the loadMain function is run.
- */
-window.onload = loadMain;
-
-/**
- * getEventTarget - Gets the target for the event.
- *
-  * http://stackoverflow.com/questions/5116929/get-clicked-li-from-ul-onclick
- * @param  {event} e event
- * @return {DOM element}   event target
- */
+// Gets the target for an event
+// http://stackoverflow.com/questions/5116929/get-clicked-li-from-ul-onclick
 function getEventTarget(e) {
-    e = e || window.event;
-    return e.target || e.srcElement;
+  e = e || window.event;
+  return e.target || e.srcElement;
 }
 
-/**
- * loadMain - Loads the home page.
- */
-function loadMain() {
-    var inner = '<object type="text/html" data=home.html></object>';
-    document.getElementById("content").innerHTML = inner;
+// DOM elements
+var mainContainer = document.getElementById('mainContainer');
+var modalBox = document.getElementById('modalBox');
+var slideImage = document.getElementById("slideImage");
+var prevBtn = document.getElementById("prevBtn");
+var nextBtn = document.getElementById("nextBtn");
+var images = document.getElementsByClassName('image');
+
+// Show slide show when an image is clicked
+for (var i = 0; i < images.length; i++) {
+  images[i].onclick = function() {
+    showSlideImage(this);
+    // Make the background content static when the slide show is active
+    mainContainer.style.position = "fixed";
+  }
 }
 
-/**
- * ul.onClick - onClick-handler for ul.
- *
- * @param  {event} event the click event.
- */
-ul.onclick = function(event) {
-    var li = getEventTarget(event);
-    if (li === null) {
-        console.log("List item was not found in the navigation bar.");
-    } else {
-        var url = li.dataset.url;
-        var inner = '<object type="text/html" data=' + url + '.html></object>';
-        document.getElementById("content").innerHTML = inner;
-    }
-};
+// Hide the slide show when we click anywhere in the screen
+modalBox.onclick = function(event) {
+  // If "previous" or "next" button: do nothing
+  var target = getEventTarget(event);
+  if (target == prevBtn || target == nextBtn) {
+    return;
+  }
+
+  // Hide the slide show and make the background content dynamic
+  modalBox.style.display = "none";
+  mainContainer.style.position = "relative";
+}
+
+// Slide backwards
+prevBtn.onclick = function() {
+  var dataId = slideImage.getAttribute("data-imageid");
+  var currentId = parseInt(dataId);
+  if (currentId == 1) {
+    currentId = images.length + 1;
+  }
+
+  slide(currentId - 1);
+}
+
+// SLide forwards
+nextBtn.onclick = function() {
+  var dataId = slideImage.getAttribute("data-imageid");
+  var currentId = parseInt(dataId);
+  if (currentId == images.length) {
+    currentId = 0;
+  }
+
+  slide(currentId + 1);
+}
+
+// Performs the slide by changing the image source
+function slide(id) {
+  id = String(id);
+  var image = document.getElementById(id);
+  showSlideImage(image);
+}
+
+// Shows the chosen image in the slide show
+function showSlideImage(image) {
+  modalBox.style.display = "block";
+  slideImage.src = image.src;
+  // Set the slide show image id equal to the source image "data-id".
+  slideImage.setAttribute("data-imageid", image.id);
+}
