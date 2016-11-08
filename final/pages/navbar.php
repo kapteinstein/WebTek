36e157fd744362e61d43993d26b491605a0f46db
Erik Liodden <eriklio@stud.ntnu.no>
2016-11-08 17:58:08 +0100

    edit all pages to navbar

--- final/pages/navbar.php
+++ final/pages/navbar.php
@@ -4,24 +4,24 @@ echo '
       <div class="submenu">
         <a href="cinematics.php">Cinematics</a>
         <a href="triptyk.php">Triptyk</a>
-        <a href="flukt.html">Flukt</a>
-        <a href="glod.html">Glød</a>
+        <a href="flukt.php">Flukt</a>
+        <a href="glod.php">Glød</a>
       </div>
       <div class="dropdown">
-        <a href="main.html"><span id="author">Daniel Bolstad</span></a>
+        <a href="main.php"><span id="author">Daniel Bolstad</span></a>
         <div class="dropdown-menu">
           <div class="dropdown-menu-items">
-            <a href="bok.html">Bok</a>
-            <a href="kontakt.html">Kontakt</a>
-            <a href="bio.html">Biografi</a>
+            <a href="bok.php">Bok</a>
+            <a href="kontakt.php">Kontakt</a>
+            <a href="bio.php">Biografi</a>
           </div>
         </div>
       </div>
       <div class="submenu">
-        <a href="pandora.html">Pandora</a>
-        <a href="icarus.html">Icarus</a>
-        <a href="matthew.html">Matthew</a>
-        <a href="kings.html">Kings</a>
+        <a href="pandora.php">Pandora</a>
+        <a href="icarus.php">Icarus</a>
+        <a href="matthew.php">Matthew</a>
+        <a href="kings.php">Kings</a>
       </div>
     </div>
     <hr />';
