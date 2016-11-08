f4b7cd17a8db046b5d2f0cd53b6db78d05026130
Erik Liodden <eriklio@stud.ntnu.no>
2016-11-08 17:32:04 +0100

    try header

--- final/pages/cinematics.php
+++ final/pages/cinematics.php
@@ -2,12 +2,7 @@
 <html>
 
 <head>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" type="text/css" href="../css/styles.css" />
-  <link rel="stylesheet" type="text/css" href="../css/navbar.css" />
-  <script src="../js/main.js" async></script>
-  <meta name=keywords content="fotografi, kunst, kunstfoto" />
+  <?php include "header.php";?>
   <title>Cinematics</title>
 </head>
 
